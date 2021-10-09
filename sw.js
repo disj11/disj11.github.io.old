087a9e20ff042e1b05481d3cfdf5cbc7cd0f00f2
disj11 <disj11@naver.com>
2021-10-09 13:36:22 +0000

    Deploying to master from @ disj11/disj11.github.io@788eff4f0670cfa25b2fa7199b0a463aeb78e21d 🚀

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8eff590a00422f7a318b.js"
+    "url": "webpack-runtime-8c2950e35e870f10b073.js"
   },
   {
-    "url": "styles.6e4fdc2f3b1b83a07768.css"
+    "url": "styles.82d3cdbb38d5d366f0db.css"
   },
   {
     "url": "styles-9605c3fbd8ad08ecf405.js"
@@ -38,14 +38,14 @@ self.__precacheManifest = [
     "url": "framework-727eca7a674e10b21a9f.js"
   },
   {
-    "url": "app-1118d7ca66b16d954ad6.js"
+    "url": "app-a5b0145839e576690722.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-38fe08ef875e5952d5f0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f2d0035fbfbcc241fdb10b447b8e55ac"
+    "revision": "664303e0cd47e791d18cd0769c3d0737"
   },
   {
     "url": "manifest.webmanifest",
@@ -73,7 +73,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-1118d7ca66b16d954ad6.js`))) {
+  if (!resources || !(await caches.match(`/app-a5b0145839e576690722.js`))) {
     return await fetch(event.request)
   }
 
