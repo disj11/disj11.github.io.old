04a8113b1b3b4a8f3b4749caa50471fd5e2c6481
disj11 <disj11@naver.com>
2021-10-15 15:33:58 +0900

    ga4 플러그인으로 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,118 +2,124 @@ require("dotenv").config()
 const metaConfig = require('./gatsby-meta-config')
 
 module.exports = {
-  siteMetadata: metaConfig,
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/__about`,
-        name: `about`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-katex`,
+    siteMetadata: metaConfig,
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              strict: `ignore`,
+                path: `${__dirname}/content/blog`,
+                name: `blog`,
             },
-          },
-          {
-            resolve: `gatsby-remark-images`,
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 1200,
-              linkImagesToOriginal: false,
+                path: `${__dirname}/content/__about`,
+                name: `about`,
             },
-          },
-          {
-            resolve: `gatsby-remark-images-medium-zoom`,
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              margin: 36,
-              scrollOffset: 0,
+                path: `${__dirname}/content/assets`,
+                name: `assets`,
             },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-katex`,
+                        options: {
+                            strict: `ignore`,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1200,
+                            linkImagesToOriginal: false,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-images-medium-zoom`,
+                        options: {
+                            margin: 36,
+                            scrollOffset: 0,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            inlineCodeMarker: '%',
+                        },
+                    },
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-smartypants`,
+                    `gatsby-remark-autolink-headers`,
+                    `gatsby-remark-emoji`,
+                ],
             },
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
+        },
+        {
+            resolve: `gatsby-plugin-google-gtag`,
             options: {
-              inlineCodeMarker: '%',
+                trackingIds: [
+                    metaConfig.ga,
+                ],
+                gtagConfig: {
+                    anonymize_ip: true,
+                },
+                pluginConfig: {
+                    head: true,
+                }
             },
-          },
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-          `gatsby-remark-autolink-headers`,
-          `gatsby-remark-emoji`,
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: metaConfig.ga,
-        head: true,
-        anonymize: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: metaConfig.title,
-        short_name: metaConfig.title,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: metaConfig.icon,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://disj11.github.io',
-        sitemap: 'https://disj11.github.io/sitemap/sitemap-index.xml',
-        policy: [
-          {
-            userAgent: '*',
-            allow: '/',
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: metaConfig.title,
+                short_name: metaConfig.title,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: metaConfig.icon,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography`,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-robots-txt',
+            options: {
+                host: 'https://disj11.github.io',
+                sitemap: 'https://disj11.github.io/sitemap/sitemap-index.xml',
+                policy: [
+                    {
+                        userAgent: '*',
+                        allow: '/',
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
           {
             site {
               siteMetadata {
@@ -125,20 +131,20 @@ module.exports = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
-            },
-            query: `
+                feeds: [
+                    {
+                        serialize: ({query: {site, allMarkdownRemark}}) => {
+                            return allMarkdownRemark.edges.map(edge => {
+                                return Object.assign({}, edge.node.frontmatter, {
+                                    description: edge.node.excerpt,
+                                    date: edge.node.frontmatter.date,
+                                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    custom_elements: [{"content:encoded": edge.node.html}],
+                                })
+                            })
+                        },
+                        query: `
               {
                 allMarkdownRemark(
                   sort: { order: DESC, fields: [frontmatter___date] },
@@ -157,38 +163,38 @@ module.exports = {
                 }
               }
             `,
-            output: "/rss.xml",
-            title: "Dev Logs RSS Feed",
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-lodash`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
-        // Tip: use Search API key with GATSBY_ prefix to access the service from within components
-        apiKey: process.env.ALGOLIA_API_KEY,
-        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME, // for all queries
-        queries: require("./src/utils/algolia-queries"),
-        chunkSize: 10000, // default: 1000
-        settings: {
-          // optional, any index settings
-          // Note: by supplying settings, you will overwrite all existing settings on the index
+                        output: "/rss.xml",
+                        title: "Dev Logs RSS Feed",
+                    },
+                ],
+            },
         },
-        enablePartialUpdates: true, // default: false
-        matchFields: ['slug', 'modified'], // Array<String> default: ['modified']
-        concurrentQueries: false, // default: true
-        skipIndexing: true, // default: false, useful for e.g. preview deploys or local development
-        continueOnFailure: false, // default: false, don't fail the build if algolia indexing fails
-      },
-    },
-  ],
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-lodash`,
+        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-plugin-algolia`,
+            options: {
+                appId: process.env.GATSBY_ALGOLIA_APP_ID,
+                // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
+                // Tip: use Search API key with GATSBY_ prefix to access the service from within components
+                apiKey: process.env.ALGOLIA_API_KEY,
+                indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME, // for all queries
+                queries: require("./src/utils/algolia-queries"),
+                chunkSize: 10000, // default: 1000
+                settings: {
+                    // optional, any index settings
+                    // Note: by supplying settings, you will overwrite all existing settings on the index
+                },
+                enablePartialUpdates: true, // default: false
+                matchFields: ['slug', 'modified'], // Array<String> default: ['modified']
+                concurrentQueries: false, // default: true
+                skipIndexing: true, // default: false, useful for e.g. preview deploys or local development
+                continueOnFailure: false, // default: false, don't fail the build if algolia indexing fails
+            },
+        },
+    ],
 }
