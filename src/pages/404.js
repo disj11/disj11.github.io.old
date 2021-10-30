ab81cf27d0d61f630687ed64eb54ebe897824f79
disj11 <disj11@naver.com>
2021-10-30 09:57:09 +0900

    404 페이지 변경

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import {graphql} from 'gatsby'
 
-import { Layout } from '../layout'
-import { Head } from '../components/head'
+import {Layout} from '../layout'
+import {Head} from '../components/head'
+import {Search as SearchIcon} from "@styled-icons/fa-solid/Search";
 
 class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+    render() {
+        const {data} = this.props
+        const siteTitle = data.site.siteMetadata.title
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <Head title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    )
-  }
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <Head title="404: Not Found"/>
+                <h1>Not Found</h1>
+                <div>
+                    페이지를 찾을 수 없다면 상단바의
+                    <SearchIcon className="search-icon" size="16" style={{marginLeft: 5, marginRight: 5}}/>
+                    을 사용해보세요!
+                </div>
+            </Layout>
+        )
+    }
 }
 
 export default NotFoundPage
